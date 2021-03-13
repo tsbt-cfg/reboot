0c23b4265bb8d1929306a901b40992db34167a71
eugenioesg <eugenio@esg.eng.br>
2021-03-13 16:33:37 -0300

    enable script

---
+++ enable.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if /usr/bin/snap enable influxdb-configurable; then
+echo influxdb enabled
+else
+echo influxdb already enabled
+fi
+
+if /usr/bin/snap enable tsbt; then
+echo tsbt enabled
+else
+echo tsbt already enabled
+fi
