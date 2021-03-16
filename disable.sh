e06da40f0c4e79befd188425705318cafbda6ce2
tsbt-cfg <70200260+tsbt-cfg@users.noreply.github.com>
2021-03-16 19:26:03 -0300

    reboot.timer delay
    make time not to permit reboot.timer reboot again by the clock

--- disable.sh
+++ disable.sh
@@ -10,7 +10,7 @@ echo tsbt disabled
 else
 echo tsbt already disabled
 fi
-
+sleep 1m #make time not to permit reboot.timer reboot again by the clock
 sudo dbus-send --system --print-reply \
 --dest=org.freedesktop.login1 /org/freedesktop/login1 \
 "org.freedesktop.login1.Manager.Reboot" boolean:true
