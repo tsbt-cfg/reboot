8eadc2d556e35187edd7182bebbcfa2c820a1607
eugenioesg <eugenio@esg.eng.br>
2021-03-17 21:35:30 -0300

    reformat if

--- unload.sh
+++ unload.sh
@@ -4,7 +4,7 @@ NOTIFY="2.5"
 FTEXT='load average:'
 LOAD5MIN="$(uptime | awk -F "$FTEXT" '{ print $2 }' | cut -d, -f2 | sed 's/ //g')"
 
-if [ expr $LOAD5MIN \> $NOTIFY ];then
+if expr $LOAD5MIN \> $NOTIFY ;then
 echo "High load 5 min" $LOAD5MIN
 /usr/bin/snap disable tsbt
 /usr/bin/snap disable influxdb-configurable
