cdd7453f20163f0863e80af78eed6f13d756d184
eugenioesg <eugenio@esg.eng.br>
2021-03-23 11:33:40 -0300

    nmap.nping

--- reconnectInternet.sh
+++ reconnectInternet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-CONNECTION=$(speed-test --json)
+CONNECTION=$(nmap.nping "noip.com")
 IGNORECASE=1
-ISNOTCONNECTED=`echo $CONNECTION | awk -F[e,E]rror '{ print $2 }'| sed 's/ //g'`
+ISCONNECTED=`echo $CONNECTION | awk -F[f,F]'ailed: 0' '{ print $2 }'| sed 's/ //g'`
 
-if ! [ -z "$ISNOTCONNECTED" ];then # test an empty variable
+if  [ -z "$ISCONNECTED" ];then # test for Failed: 0
 echo $CONNECTION
 ./bin/disable.sh
 fi
