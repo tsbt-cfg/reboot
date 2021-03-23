68e2772950085492df4d9e2940205f2ab47b1c10
eugenioesg <eugenio@esg.eng.br>
2021-03-23 13:53:39 -0300

    simplify connection test to noip

--- reconnectInternet.sh
+++ reconnectInternet.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 CONNECTION=$(nping "noip.com")
-IGNORECASE=1
 ISCONNECTED=`echo $CONNECTION | awk -F[f,F]'ailed: 0' '{ print $2 }'| sed 's/ //g'`
 
-if  [ -z "$ISCONNECTED" ];then # test for Failed: 0
-echo $CONNECTION
-./bin/disable.sh
-fi
+test $ISCONNECTED && echo $CONNECTION || ./bin/disable.sh
