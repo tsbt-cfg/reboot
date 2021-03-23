548dca81ef384d3065de86995600718c6dbb09b0
tsbt-cfg <70200260+tsbt-cfg@users.noreply.github.com>
2021-03-23 19:36:51 -0300

    log connection

--- reconnectInternet.sh
+++ reconnectInternet.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+echo "tested connection" > tty2
 CONNECTION=$(nping "noip.com")
 ISCONNECTED=`echo $CONNECTION | awk -F[f,F]'ailed: 0' '{ print $2 }'| sed 's/ //g'`
 
-test $ISCONNECTED && echo $CONNECTION > /dev/tty2 || disable.sh
+test $ISCONNECTED && echo $CONNECTION || disable.sh
