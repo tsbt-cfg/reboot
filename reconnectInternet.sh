8d834ce4e0af53b76ac8648a4fecc89ae800fd34
tsbt-cfg <70200260+tsbt-cfg@users.noreply.github.com>
2021-03-23 12:57:52 -0300

    correct is connect logic

--- reconnectInternet.sh
+++ reconnectInternet.sh
@@ -3,7 +3,7 @@ CONNECTION=$(nping "noip.com")
 IGNORECASE=1
 ISCONNECTED=`echo $CONNECTION | awk -F[f,F]'ailed: 0' '{ print $2 }'| sed 's/ //g'`
 
-if  [ -z "$ISCONNECTED" ];then # test for Failed: 0
+if  ![ -z "$ISCONNECTED" ];then # test for Failed: 0
 echo $CONNECTION
 ./bin/disable.sh
 fi
