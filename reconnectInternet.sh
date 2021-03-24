f97bf7ed39d3ddc8f776570a71aa0c24771a69a4
tsbt-cfg <70200260+tsbt-cfg@users.noreply.github.com>
2021-03-24 18:12:39 -0300

    shutdown if uptime gt 1h and is not connected

--- reconnectInternet.sh
+++ reconnectInternet.sh
@@ -4,4 +4,5 @@ CONNECTION=$(nping "noip.com")
 ISCONNECTED=`echo $CONNECTION | awk -F[f,F]'ailed: 0' '{ print $2 }'| sed 's/ //g'`
 UPTIMELT60MIN=$(uptime | awk -F'min' '{print$2}' | sed 's/ //g')
 test $ISCONNECTED && echo "internet connected" > /dev/tty2 || echo "internet disconnected" > /dev/tty2
-test $ISCONNECTED && test $UPTIMELT60MIN && echo $CONNECTION || disable.sh
+RESULT=$(test $UPTIMELT60MIN && echo '' || echo $ISCONNECTED)
+test $RESULT && echo '' || disable.sh
