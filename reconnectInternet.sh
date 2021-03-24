05295b1e66dcbad65172ef604df60281c5f11585
eugenioesg <eugenio@esg.eng.br>
2021-03-24 17:01:22 -0300

    in progress

--- reconnectInternet.sh
+++ reconnectInternet.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "tested connection" > tty2
+
 CONNECTION=$(nping "noip.com")
 ISCONNECTED=`echo $CONNECTION | awk -F[f,F]'ailed: 0' '{ print $2 }'| sed 's/ //g'`
-
-test $ISCONNECTED && echo $CONNECTION || disable.sh
+UPTIMELT60MIN=$(uptime | awk -F'min' '{print$2}' | sed 's/ //g')
+test $ISCONNECTED && echo "internet connected" > /dev/tty2 || echo "internet disconnected" > /dev/tty2
+test $ISCONNECTED && test $UPTIMELT60MIN && echo $CONNECTION || disable.sh
