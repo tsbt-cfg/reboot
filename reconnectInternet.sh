601a5a958bcf8c0ee42ae20215dbf4147930f1ec
tsbt-cfg <70200260+tsbt-cfg@users.noreply.github.com>
2021-03-24 18:17:14 -0300

    redo logic

--- reconnectInternet.sh
+++ reconnectInternet.sh
@@ -4,5 +4,5 @@ CONNECTION=$(nping "noip.com")
 ISCONNECTED=`echo $CONNECTION | awk -F[f,F]'ailed: 0' '{ print $2 }'| sed 's/ //g'`
 UPTIMELT60MIN=$(uptime | awk -F'min' '{print$2}' | sed 's/ //g')
 test $ISCONNECTED && echo "internet connected" > /dev/tty2 || echo "internet disconnected" > /dev/tty2
-RESULT=$(test $UPTIMELT60MIN && echo '' || echo $ISCONNECTED)
+RESULT=$(test $UPTIMELT60MIN && echo 'not lt 60' || echo $ISCONNECTED)
 test $RESULT && echo '' || disable.sh
