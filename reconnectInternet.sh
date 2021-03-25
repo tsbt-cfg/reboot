e639cc40d462ec0e581b5bf0537548b9d3e305f5
eugenioesg <eugenio@esg.eng.br>
2021-03-24 21:28:32 -0300

    search for lost: 0

--- reconnectInternet.sh
+++ reconnectInternet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CONNECTION=$(nping "noip.com")
-ISCONNECTED=`echo $CONNECTION | awk -F[f,F]'ailed: 0' '{ print $2 }'| sed 's/ //g'`
+CONNECTION=$(nping -v0 -d0 "noip.com")
+ISCONNECTED=`echo $CONNECTION | awk -F[l,L]'ost: 0' '{ print $2 }'| sed 's/ //g'`
 UPTIMELT60MIN=$(uptime | awk -F'min' '{print$2}' | sed 's/ //g')
 test $ISCONNECTED && echo "internet connected" > /dev/tty2 || echo "internet disconnected" > /dev/tty2
 RESULT=$(test $UPTIMELT60MIN && echo 'not lt 60' || echo $ISCONNECTED)
