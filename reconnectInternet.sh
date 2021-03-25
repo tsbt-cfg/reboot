4f12be1aa6eeff21381261848515cf43672880a2
tsbt-cfg <70200260+tsbt-cfg@users.noreply.github.com>
2021-03-25 08:53:08 -0300

    wait for reboot

--- reconnectInternet.sh
+++ reconnectInternet.sh
@@ -4,7 +4,7 @@ CONNECTION=$(nping -v0 -d0 "noip.com")
 ISCONNECTED=`echo $CONNECTION | awk -F[l,L]'ost: 0' '{ print $2 }'| sed 's/ //g'`
 UPTIMELT60MIN=$(uptime | awk -F'min' '{print$2}' | sed 's/ //g')
 test $ISCONNECTED && echo "internet connected" > /dev/tty2 || echo "internet disconnected" > /dev/tty2
-RESULT=$(test $UPTIMELT60MIN && echo 'bellow 1 hour' || echo $ISCONNECTED)
+RESULT=$(test $UPTIMELT60MIN && echo 'WAITFORREBOOT' || echo $ISCONNECTED)
 test $RESULT && echo '' || disable.sh
 
 
