42a34bcdae7a784b00a6fa25412a490d300b2c9a
tsbt-cfg <70200260+tsbt-cfg@users.noreply.github.com>
2021-04-04 16:01:08 -0300

    use tty0 and print rpi temperature

--- unload.sh
+++ unload.sh
@@ -2,7 +2,8 @@
 NOTIFY="2.5"
 
 FTEXT='load average:'
-`uptime > /dev/tty2`
+`uptime > /dev/tty0`
+cat /sys/class/thermal/thermal_zone0/temp > /dev/tty0
 LOAD5MIN="$(uptime | awk -F "$FTEXT" '{ print $2 }' | cut -d, -f2 | sed 's/ //g')"
 
 if expr $LOAD5MIN \> $NOTIFY ;then
