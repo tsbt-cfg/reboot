a1c9bb519b7b15115819b5b55659062b6883b243
tsbt-cfg <70200260+tsbt-cfg@users.noreply.github.com>
2021-04-05 09:11:12 -0300

    tty1 and tty2

--- unload.sh
+++ unload.sh
@@ -2,8 +2,12 @@
 NOTIFY="2.5"
 
 FTEXT='load average:'
-`uptime > /dev/tty0`
-cat /sys/class/thermal/thermal_zone0/temp > /dev/tty0
+`uptime > /dev/tty1`
+cat /sys/class/thermal/thermal_zone0/temp > /dev/tty1
+`uptime > /dev/tty2`
+cat /sys/class/thermal/thermal_zone0/temp > /dev/tty2
+
+
 LOAD5MIN="$(uptime | awk -F "$FTEXT" '{ print $2 }' | cut -d, -f2 | sed 's/ //g')"
 
 if expr $LOAD5MIN \> $NOTIFY ;then
